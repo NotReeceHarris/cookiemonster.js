8d79eeeebbfd9ff94331afa9dfbd5ac46a69abbe
Reece Harris <reece.harris98@protonmail.com>
2022-01-20 22:38:44 +0000

    test

--- src/cookieMonster.js
+++ src/cookieMonster.js
@@ -1,1 +1,4 @@
 console.log('This is a test to minify build');
+console.log('This is a test to minify build');
+console.log('This is a test to minify build');
+console.log('This is a test to minify build');
